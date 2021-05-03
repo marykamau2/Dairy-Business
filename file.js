45f774b95e356fb8005adb606bfa51f973723b29
marykamau2 <mary.kamau@student.moringaschool.com>
2021-05-03 08:04:04 +0300

    added the production id

--- file.js
+++ file.js
@@ -5,4 +5,19 @@ function mary() {
     for (var i = 0; i < items.length; i++) { 
         var a = items[i]; 
         no = no +  a.value + " " +"<br>"; 
- } };
+ } 
+ document.getElementById("res").innerHTML = no; 
+    document.getElementById("milk").innerHTML = "Productions"; 
+} 
+
+var totalLitres = 0;
+function totalCapacity(){
+for(var i = 0; i < items.length; i++)
+{
+    totalLitres = totalLitres + items[i];
+}
+
+}
+
+var dailyIncome;
+var rate = 45;
